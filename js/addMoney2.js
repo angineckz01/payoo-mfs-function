62948ce2f19252fdf1b2ad2170479771ac5e6fde
Your Name <angineckz01@gmail.com>
2025-03-17 20:03:58 +0600

    added transaction history

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -8,6 +8,17 @@ document.getElementById('button-add-money').addEventListener('click', function(e
         const balance = textValueById('account-balance');
         const updateBalance = balance + addMoney;
         document.getElementById('account-balance').innerText = updateBalance;
+
+        // ADD TO TRANSACTION HISTORY HERE
+        const p = document.createElement('p');
+        p.classList.add('bg-gray-400');
+        p.innerHTML = `
+            <h3 class="text-2xl font-bold">Add Money</h3>
+            <p>Added ${addMoney} taka. New Balance ${updateBalance} taka</P>
+        `
+        const addMoneyHistory = document.getElementById('add-money-history');
+        // addMoneyHistory.classList.add('space-y-4');
+        addMoneyHistory.appendChild(p);
     }
     else{
         alert('Failed to add the money. Please try again later.')
--- js/cashout2.js
+++ js/cashout2.js
@@ -7,6 +7,16 @@ document.getElementById('button-cash-out').addEventListener('click', function(ev
         const updateBalance = balance - inputCashoutMoney;
         // update the new balance
         document.getElementById('account-balance').innerText = updateBalance;
+
+        // TRANSACTION HISTORY BEGINS HERE
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h2 class="text-2xl font-bold">Cash Out</h2>
+            <p>${inputCashoutMoney} cashout. New balance ${updateBalance}</p>
+        `
+        const cashoutHistory = document.getElementById('cash-out-history');
+        cashoutHistory.appendChild(div);
     }
     else{
         alert('failed to cash out money. please try again later.');
